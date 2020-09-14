44397cd15bdd68d7c7e837ad46adacee00841f7a
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-09-14 22:24:15 +0800

    [fengyi_ling] feat:complete matchers

--- src/matchers/__test__/expect.calls.test.js
+++ src/matchers/__test__/expect.calls.test.js
@@ -2,11 +2,17 @@ function forEach(items, callback) {
   items.forEach((item) => callback(item));
 }
 
+// eslint-disable-next-line jest/expect-expect
 test("TODO 11", () => {
   const mockCallback = jest.fn((x) => 42 + x);
   forEach([1, 2], mockCallback);
 
   // TODO 11: add assertion
+  // eslint-disable-next-line jest/valid-expect
+  expect(mockCallback).toHaveBeenCalled();
+  expect(mockCallback).toHaveBeenCalledTimes(2);
+  expect(mockCallback).toHaveBeenCalledWith(1);
+  expect(mockCallback).toHaveBeenCalledWith(2);
 });
 
 test("TODO 12", () => {
@@ -17,8 +23,8 @@ test("TODO 12", () => {
     .mockReturnValue(true);
 
   // TODO 12: to add "expected" value
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
+  expect(mockFn()).toBe(42);
+  expect(mockFn()).toBe("string");
+  expect(mockFn()).toBe(true);
+  expect(mockFn()).toBe(true);
 });
